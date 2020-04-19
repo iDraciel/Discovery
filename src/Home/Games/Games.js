76bf241c343e131c3f1f05186736b09bc8279d3e
Purushottam <iDraciel@users.noreply.github.com>
2020-04-19 17:16:36 +0530

    added routing

--- src/Home/Games/Games.js
+++ src/Home/Games/Games.js
@@ -4,14 +4,22 @@ import DOTA from './DOTA-Game.jpg';
 import PUBG from './PUBG-Game.jpg';
 import LOL from './LOL-Game.jpg';
 import './Games.css';
-
+import Card from '../../Gamecard.js';
+import {BrowserRouter as Router,Route ,Link} from 'react-router-dom';
 class Game extends Component{
        
     render(){
-  return( <div className="flex-container">
+    
+  return(
+    <div>
+      <h1>Top Games</h1>
+    <div className="game-content"> <div className="flex-container">
           <div className="card">
       <img src={DOTA} alt="DOTA" className="pic"/>
-     <button className="button button2">Learn More</button>
+      
+      <button className="button button2"><Link to="Card">Tutorial</Link></button>
+      
+    
      <button className="button button1">Play</button>
     </div>
        <div className="card">
@@ -29,6 +37,10 @@ class Game extends Component{
      <button className="button button2">Learn More</button>
      <button className="button button1">Play</button>
        </div>
+       </div>
+       </div>
+
+
        </div>
   );
 }
