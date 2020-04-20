a3f7b1414f9039534d55004a7c06d63511c83726
Purushottam <iDraciel@users.noreply.github.com>
2020-04-20 21:13:04 +0530

    fixed codes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import About from './About/About.js';
 import Nav from "./Navbar/Nav.js";
 import Home from "./Home/Home.js";
 import Footer from './footer/Footer.js';
-<<<<<<< HEAD
 import GamePage from './GamePage/GamePage.js';
-=======
-import GamePage from './GamePage/GamePage';
->>>>>>> 9d3d71e65d12418d95bdfe32adebe725fa6c26d1
 import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
@@ -20,17 +16,9 @@ import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
       <Nav/>
       
     <Switch>
-<<<<<<< HEAD
      <Route path="/" exact component={Home}/>
      <Route path="/Home" exact component={Home}/>
      <Route path="/about" component={About}/>
-=======
-     <Route path="/home" exact component={Home}/>
-     <Route path="/about" component={About}/>
-     <Route path="/:id" component={GamePage}/>
-     <Route path="*" component={Home}/>
-     </Switch>
->>>>>>> 9d3d71e65d12418d95bdfe32adebe725fa6c26d1
      
      <Route path="/:id" component={GamePage}/>
      <Route path="*" component={ErrorPage}/>
