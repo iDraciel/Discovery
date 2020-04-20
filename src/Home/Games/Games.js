9d3d71e65d12418d95bdfe32adebe725fa6c26d1
akhilrajarj <raj30.akhil@gmail.com>
2020-04-20 12:23:28 +0530

    Video URLS added

--- src/Home/Games/Games.js
+++ src/Home/Games/Games.js
@@ -15,15 +15,32 @@ class Game extends Component{
     <div className="game-content"> <div className="flex-container">
           <div className="card">
       <img src={DOTA} alt="DOTA" className="pic"/>
-      
-      <button className="button button2"><Link to="Card">Tutorial</Link></button>
+      <Link to={{
+        pathname:'/DOTA-2',
+        state:{
+          video1:"https://www.youtube.com/embed/9Szj-CloJiI",
+          video2:"https://www.youtube.com/embed/u0rYxCVRrUM",
+          video3:"https://www.youtube.com/embed/0qNh-wKkJGU"
+        }
+      }}>
+      <button className="button button2">Learn More</button>
+      </Link>
       
     
      <button className="button button1">Play</button>
     </div>
        <div className="card">
       <img src={CSGO} alt="CSGO" className="pic"/>
+      <Link to={{
+         pathname:'/CSGO',
+         state:{
+          video1:"https://www.youtube.com/embed/eFab7sTSmEc",
+          video2:"https://www.youtube.com/embed/-7lTNsgnjuo",
+          video3:"https://www.youtube.com/embed/GidC3dWlVUA"
+        }
+      }}>
      <button className="button button2">Learn More</button>
+     </Link>
      <button className="button button1">Play</button>
        </div>>
       
@@ -32,9 +49,9 @@ class Game extends Component{
          <Link to={{
            pathname:"/PUBG",
            state:{
-             video1:"https://www.youtube.com/embed/tgbNymZ7vqY",
-             video2:"https://www.youtube.com/embed/tgbNymZ7vqY",
-             video3:"https://www.youtube.com/embed/tgbNymZ7vqY"
+             video1:"https://www.youtube.com/embed/N_BYaE-XW7E",
+             video2:"https://www.youtube.com/embed/N_C8u9UA4Zo",
+             video3:"https://www.youtube.com/embed/MddquVCgYGY"
            }
          }}>
        <button className="button button2">Learn More</button> </Link>
@@ -43,7 +60,16 @@ class Game extends Component{
        
      <div className="card">
       <img src={LOL} alt="LOL" className="pic"/>
+      <Link to={{
+        pathname:'/League OF Legends',
+        state:{
+          video1:"https://www.youtube.com/embed/0uyLRPmmYPk",
+          video2:"https://www.youtube.com/embed/z6lNQVpTBgI",
+          video3:"https://www.youtube.com/embed/TzfQvu98b_o"
+        }
+      }}>
      <button className="button button2">Learn More</button>
+     </Link>
      <button className="button button1">Play</button>
        </div>
        </div>
